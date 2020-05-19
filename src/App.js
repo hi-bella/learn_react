f17097d113d2bd2d4153c0ab7c62417d1dfa8f7f
bella-tnis <bella.li@tnis.com>
2020-05-19 16:08:54 +0700

    add person functional component, add handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm Bella</h1>
-    </div>
-  );
+// class components
+class App extends Component {
+  state = {
+    persons : [
+      {name:"Bella", age:23},
+      {name:"Caroline", age:27},
+      {name:"Margaret", age:31},
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons : [
+        {name:"Bellaaaaa", age:23},
+        {name:"Caroline", age:27},
+        {name:"Margaret", age:32},
+      ]
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hi, I'm Bella</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Reading</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+      //React.createElement("div", {className: 'App'}, React.createElement("h1", null, "Does this work now?"))
+    );
+  } 
 }
+
+// functional component using react hooks
 
 export default App;
