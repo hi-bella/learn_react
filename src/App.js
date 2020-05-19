4f3473a4922cb09592e716861a0d1d71ac317eee
bella-tnis <bella.li@tnis.com>
2020-05-19 11:45:30 +0700

    first commit react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I'm Bella</h1>
+    </div>
+  );
+}
+
+export default App;
